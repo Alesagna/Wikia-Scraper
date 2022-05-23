a8068becd64ac421af6c26b7f188b8db0893721a
Ale <105257087+Alesagna@users.noreply.github.com>
2022-05-22 19:42:30 -0500

    Add files via upload

---
+++ scrape.py
@@ -0,0 +1,25 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+
+
+def Scraper(url):
+    os.chdir(os.path.join(os.getcwd(), "Saved Images"))
+    request = requests.get(url)
+    soup = BeautifulSoup(request.text, "html.parser")
+    images = soup.find("div", class_="mw-parser-output").find_all("img", class_="thumbimage lazyload")
+
+    for image in images:
+        preImageLink = image["data-src"]
+        imageKey = image["data-image-key"]
+        subImageLink1 = preImageLink.split("/scale-to-width-down/", 1)
+        substring1 = subImageLink1[0]
+        subImageLink2 = url.split("?", 1)
+        substring2 = subImageLink2[0]
+        imageLink = (f"{substring1}?{substring2}")
+        title = image["alt"]
+        with open(title.replace(":", "-").replace("?", "-") + imageKey[-4:], "wb") as f:
+            img = requests.get(imageLink)
+            f.write(img.content)
+
+
